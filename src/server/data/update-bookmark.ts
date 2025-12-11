ef53bb7b4bb8760d36ae296adb69d1586f6b4dc2
Renan Oliveira <rotrenanoliveira@gmail.com>
2025-12-11 12:28:08 -0300

    refactor: refactoring bookmark update

--- src/server/data/update-bookmark.ts
+++ src/server/data/update-bookmark.ts
@@ -1,18 +1,30 @@
-import { prisma } from '@/lib/prisma'
+import { eq } from 'drizzle-orm'
+import { db } from '@/infra/db/drizzle'
+import { bookmarksRepository } from '@/infra/db/repositories'
 import { handle } from '@/utils/functions'
-import type { Bookmark, BookmarkUpdateInput, ResponseError } from '@/utils/types'
+import type { BookmarkUpdateInput, ResponseError } from '@/utils/types'
 
-export async function updateBookmark(data: BookmarkUpdateInput): Promise<[Bookmark, null] | [null, ResponseError]> {
+export async function updateBookmark(data: BookmarkUpdateInput): Promise<[null, null] | [null, ResponseError]> {
+  console.log('update data', data)
+
   if (data.folderId === undefined && !data.title) {
     return [null, { success: false, message: 'folderId or title is required' }]
   }
 
   const query = data.folderId !== undefined ? { folderId: data.folderId } : data.title ? { title: data.title } : {}
 
-  return await handle(
-    prisma.bookmark.update({
-      where: { id: data.bookmarkId },
-      data: query,
-    }),
+  console.log('update query', query)
+
+  const [_, queryError] = await handle(
+    db.update(bookmarksRepository).set(query).where(eq(bookmarksRepository.bookmarkId, data.bookmarkId)),
   )
+
+  console.log('result', _)
+  console.log('query error', queryError)
+
+  if (queryError) {
+    return [null, queryError]
+  }
+
+  return [null, null]
 }
