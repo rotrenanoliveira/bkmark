6807f06f86128f8b693536854ed594a4799a07f0
Renan Oliveira <rotrenanoliveira@gmail.com>
2025-12-11 12:35:52 -0300

    chore: removing console.log

--- src/server/data/update-bookmark.ts
+++ src/server/data/update-bookmark.ts
@@ -5,22 +5,15 @@ import { handle } from '@/utils/functions'
 import type { BookmarkUpdateInput, ResponseError } from '@/utils/types'
 
 export async function updateBookmark(data: BookmarkUpdateInput): Promise<[null, null] | [null, ResponseError]> {
-  console.log('update data', data)
-
   if (data.folderId === undefined && !data.title) {
     return [null, { success: false, message: 'folderId or title is required' }]
   }
 
   const query = data.folderId !== undefined ? { folderId: data.folderId } : data.title ? { title: data.title } : {}
 
-  console.log('update query', query)
-
   const [_, queryError] = await handle(
     db.update(bookmarksRepository).set(query).where(eq(bookmarksRepository.bookmarkId, data.bookmarkId)),
   )
-
-  console.log('result', _)
-  console.log('query error', queryError)
 
   if (queryError) {
     return [null, queryError]
