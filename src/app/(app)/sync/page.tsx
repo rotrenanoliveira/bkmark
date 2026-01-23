574fcfdb37a79a6ab50ead576a074b0c91513712
Renan Oliveira <rotrenanoliveira@gmail.com>
2026-01-23 10:56:43 -0300

    feat: add synchronize page content in a wrapper component

--- src/app/(app)/sync/page.tsx
+++ src/app/(app)/sync/page.tsx
@@ -1,11 +1,7 @@
-import { QrCodeIcon, RefreshCcwIcon, TicketIcon, ViewIcon } from 'lucide-react'
 import { redirect } from 'next/navigation'
 import { Header } from '@/components/header'
-import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
 import { getUserId } from '@/server/data/get-user-id'
-import { CopyAccessCode } from './_components/copy-access-code'
-import { AccessCodeQrCode } from './_components/qrcode-access-code'
-import { SyncForm } from './_components/sync-form'
+import { SynchronizePageContent } from './_components/synchronize-page-content'
 
 export default async function SynchronizePage() {
   const userId = await getUserId()
@@ -21,70 +17,8 @@ export default async function SynchronizePage() {
   return (
     <div className="flex flex-col min-h-screen w-screen">
       <Header />
-      <main className="w-full flex flex-col items-center py-6 space-y-4">
-        <div className="w-full max-w-sm flex flex-col gap-4 ring-foreground/10 bg-card text-card-foreground overflow-hidden rounded-xl py-4 text-sm ring-1 group/card">
-          <div className="grid auto-rows-min items-start gap-1 border-b rounded-t-xl px-4 [.border-b]:pb-4">
-            <div className="text-base leading-snug font-medium">Synchronize your bookmarks</div>
-            <div className="text-muted-foreground text-sm">
-              Use the access code below to synchronize your bookmarks with your device.
-            </div>
-          </div>
-
-          <div className="px-4 space-y-4 border-b [.border-b]:pb-4">
-            <div className="flex flex-col gap-2">
-              <div className="inline-flex items-center gap-2">
-                <div className="flex items-center justify-center p-2 bg-muted-foreground/15 rounded-3xl">
-                  <TicketIcon className="size-4 stroke-red-500" />
-                </div>
-                <p className="text-sm font-medium">Access code</p>
-              </div>
-              <div className="w-full flex py-2 px-4 items-center justify-between rounded-lg border bg-muted-foreground/5">
-                <p className="font-(family-name:--font-geist-mono)">{userId}</p>
-                <CopyAccessCode userId={userId} />
-              </div>
-            </div>
-
-            <div className="flex flex-col gap-2">
-              <div className="inline-flex items-center gap-2">
-                <div className="flex items-center justify-center p-2 bg-muted-foreground/15 rounded-3xl">
-                  <QrCodeIcon className="size-4 stroke-red-500" />
-                </div>
-                <p className="text-sm font-medium">QR Code</p>
-              </div>
-              <Popover>
-                <PopoverTrigger
-                  className="w-full flex py-2 px-4 items-center justify-between rounded-lg cursor-pointer border bg-muted-foreground/5"
-                  asChild
-                >
-                  <div className="w-full flex py-2 px-4 items-center justify-between rounded-lg border bg-muted-foreground/5">
-                    <p className="font-(family-name:--font-geist-mono)">Show</p>
-                    <div className="flex items-center justify-center size-8">
-                      <ViewIcon className="size-5" />
-                    </div>
-                  </div>
-                </PopoverTrigger>
-                <PopoverContent align="center" className="w-40">
-                  <AccessCodeQrCode syncUrl={syncUrl.toString()} />
-                </PopoverContent>
-              </Popover>
-            </div>
-          </div>
-
-          <div className="space-y-4 px-4">
-            <div className="flex flex-col gap-2 ">
-              <div className="inline-flex items-center gap-2">
-                <div className="flex items-center justify-center p-2 bg-muted-foreground/15 rounded-3xl">
-                  <RefreshCcwIcon className="size-4 stroke-blue-500" />
-                </div>
-                <p className="text-base leading-snug font-medium">Already have an access code?</p>
-              </div>
-              <span className="text-muted-foreground text-sm">
-                Enter the access code to synchronize your bookmarks.
-              </span>
-            </div>
-            <SyncForm />
-          </div>
-        </div>
+      <main className="w-full flex flex-col items-center justify-center h-[calc(100svh-72px)] py-6 space-y-4">
+        <SynchronizePageContent userId={userId} syncUrl={syncUrl.toString()} />
       </main>
     </div>
   )
