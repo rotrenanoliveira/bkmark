bb4ae692abae5e5b01e0cb0c4ae36c110ee66930
Renan Oliveira <rotrenanoliveira@gmail.com>
2026-01-24 17:36:06 -0300

    feat: add create bookmark

---
+++ src/app/(app)/@modal/(.)bookmarks/new/page.tsx
@@ -0,0 +1,5 @@
+import { AddBookmarkDialogContent } from './_components/add-bookmark-dialog-content'
+
+export default async function AddBookmarkModal() {
+  return <AddBookmarkDialogContent />
+}
--- src/components/bookmarks/bookmark-form.tsx
+++ src/components/bookmarks/bookmark-form.tsx
@@ -1,23 +1,31 @@
 'use client'
 
-import { useRef } from 'react'
-import { Plus } from 'lucide-react'
-import { toast } from 'sonner'
+import { Loader2Icon, Plus } from 'lucide-react'
 import { useRouter } from 'next/navigation'
-
+import { useRef } from 'react'
+import { toast } from 'sonner'
+import { useBookmarks } from '@/components/bookmarks/bookmarks-context'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
-import { useBookmarks } from '@/components/bookmarks/bookmarks-context'
 import { useFormState } from '@/hooks/use-form-state'
+import { cn } from '@/lib/utils'
 import { actionAddBookmark } from '@/server/actions/add-bookmark'
 
-export function BookmarkForm() {
+interface BookmarkFormProps {
+  isOnPage?: boolean
+  beforeSubmit?: () => void
+  onSuccess?: () => void
+}
+
+export function BookmarkForm(props: BookmarkFormProps) {
   const router = useRouter()
   const formRef = useRef<HTMLFormElement>(null)
 
   const { create } = useBookmarks()
 
   function optimisticFn(formData: FormData) {
+    props.beforeSubmit?.()
+
     const url = formData.get('url')?.toString()
 
     if (!url) {
@@ -30,10 +38,14 @@ export function BookmarkForm() {
 
     create({ title, bookmarkUrl: url })
 
+    if (props.isOnPage) {
+      return router.push('/')
+    }
+
     formRef.current?.reset()
   }
 
-  const [formState, handleSubmit] = useFormState(actionAddBookmark, {
+  const [formState, handleSubmit, isPending] = useFormState(actionAddBookmark, {
     onSuccess,
     onError,
     optimisticFn,
@@ -44,20 +56,24 @@ export function BookmarkForm() {
   }
 
   function onSuccess() {
+    if (props.onSuccess) {
+      return props.onSuccess()
+    }
+
     router.refresh()
   }
 
   return (
-    <div>
+    <div className="w-full">
       <form onSubmit={handleSubmit} ref={formRef} className="flex flex-col gap-2">
         {formState.success === false && <p className="text-red-500">{formState.message}</p>}
         <div className="flex gap-2">
           <Input type="text" name="url" placeholder="Enter URL" />
           {/* <Button type="submit" disabled={isPending}> */}
           <Button type="submit">
-            {/* <Plus strokeWidth={1.25} className={cn('size-5', isPending && 'hidden')} />
-            {isPending && <Loader2Icon strokeWidth={1.25} className="size-5 animate-spin" />} */}
-            <Plus strokeWidth={1.25} className="size-5" />
+            <Plus strokeWidth={1.25} className={cn('size-5', isPending && 'hidden')} />
+            {isPending && <Loader2Icon strokeWidth={1.25} className="size-5 animate-spin" />}
+            {/* <Plus strokeWidth={1.25} className="size-5" /> */}
           </Button>
         </div>
       </form>
