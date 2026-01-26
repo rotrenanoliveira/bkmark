ee0912421a9191072d9c90986f3518e42cbe1dda
Renan Oliveira <rotrenanoliveira@gmail.com>
2026-01-26 11:10:47 -0300

    refact: add bookmark to folder action removing revalidateTag from previous next.js version

--- src/server/actions/add-bookmark-to-folder.ts
+++ src/server/actions/add-bookmark-to-folder.ts
@@ -1,7 +1,6 @@
 'use server'
 
-import { revalidatePath, revalidateTag } from 'next/cache'
-
+import { revalidatePath } from 'next/cache'
 import { getBookmark } from '../data/get-bookmark'
 import { updateBookmark } from '../data/update-bookmark'
 
@@ -11,21 +10,19 @@ interface ActionProps {
 }
 
 export async function actionAddBookmarkToFolder({ bookmarkId, folderId }: ActionProps) {
-  const [currentBookmark, bookmarkError] = await getBookmark(bookmarkId)
+  const [_currentBookmark, bookmarkError] = await getBookmark(bookmarkId)
 
   if (bookmarkError) {
     return bookmarkError
   }
 
-  const [_, updateError] = await updateBookmark({ bookmarkId, folderId })
+  const [_updateResponse, updateError] = await updateBookmark({ bookmarkId, folderId })
 
   if (updateError) {
     return updateError
   }
 
   revalidatePath('/', 'layout')
-  revalidateTag(`bookmarks-${currentBookmark.folderId}`, 'max')
-  revalidateTag(`bookmarks-${folderId}`, 'max')
 
   return { success: true, message: 'Bookmark added to folder.' }
 }
