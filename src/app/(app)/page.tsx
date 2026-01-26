501d9e2a6b8054fed28bfa27ec04882d14b30c69
Renan Oliveira <rotrenanoliveira@gmail.com>
2026-01-26 16:47:38 -0300

    refact: update bookmarks components

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,33 +1,22 @@
+import { BookmarkForm } from '@/components/bookmarks/bookmark-form'
+import { UnfoldedBookmarks } from '@/components/bookmarks/unfolded-bookmarks'
+import { FolderForm } from '@/components/folders/folder-form'
+import { Folders } from '@/components/folders/folders'
 import { Header } from '@/components/header'
 
 export default async function HomePage() {
-  // const userId = await getUserId()
-  // // const appUrl = process.env.NEXT_PUBLIC_APP_URL ?? ''
-
-  // if (!userId) {
-  //   redirect('/api/sync/generate')
-  // }
-
   return (
     <div className="flex flex-col min-h-screen w-screen">
       <Header />
-      {/* <main className="w-full flex flex-col items-center py-6 space-y-4">
-        <div className="w-full max-w-4xl flex flex-col md:flex-row justify-between gap-2">
-          <BookmarkForm />
-
-          <div className="space-x-2 self-end">
-            <FolderDialog />
-            <SyncDialog userId={userId} appUrl={appUrl} />
-          </div>
-        </div>
-
+      <main className="w-full flex flex-col items-center p-6 space-y-4">
         <section className="w-full max-w-4xl grid grid-cols-1 gap-4">
+          <BookmarkForm />
+          <FolderForm />
+
           <UnfoldedBookmarks />
           <Folders />
         </section>
       </main>
-
-      <Header /> */}
     </div>
   )
 }
