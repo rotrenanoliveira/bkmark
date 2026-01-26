a779002fab31aebc6a40744a65e8489fabf55a28
Renan Oliveira <rotrenanoliveira@gmail.com>
2026-01-26 13:49:01 -0300

    refact: bookmark delete component
    Update the bookmark delete component add new ability to remove a bookmark using shortcut.
    Remove `revalidateTag` from previous Next.js version.

--- src/components/bookmarks/bookmark-delete.tsx
+++ src/components/bookmarks/bookmark-delete.tsx
@@ -1,10 +1,10 @@
+import { CircleMinusIcon, CircleXIcon } from 'lucide-react'
+import React, { useState, useTransition } from 'react'
 import { toast } from 'sonner'
-import { Trash } from 'lucide-react'
-import { useTransition } from 'react'
-
 import { useBookmarks } from '@/components/bookmarks/bookmarks-context'
 import { Button } from '@/components/ui/button'
-import { DropdownMenuItem } from '@/components/ui/dropdown-menu'
+import { DropdownMenuItem, DropdownMenuShortcut } from '@/components/ui/dropdown-menu'
+import { cn } from '@/lib/utils'
 import { actionRemoveBookmark } from '@/server/actions/remove-bookmark'
 
 interface RemoveBookmarkProps {
@@ -12,29 +12,70 @@ interface RemoveBookmarkProps {
 }
 
 export function BookmarkDeleteButton({ bookmarkId }: RemoveBookmarkProps) {
+  const [shortcutPressed, setShortcutPressed] = useState(false)
   const [isPending, startTransition] = useTransition()
+
+  const commandTimeoutRef = React.useRef<NodeJS.Timeout | undefined>(undefined)
   const { remove } = useBookmarks()
 
-  async function handleRemoveBookmark() {
-    const result = await actionRemoveBookmark(bookmarkId)
-    remove(bookmarkId)
+  // biome-ignore lint/correctness/useExhaustiveDependencies: re-render
+  React.useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if (e.metaKey && e.shiftKey && e.key.toLowerCase() === 'd') {
+        e.preventDefault()
+        setShortcutPressed(true)
+        handleRemoveBookmark()
 
-    if (result.success === false) {
-      toast.error(result.message)
-      return
+        commandTimeoutRef.current = setTimeout(() => setShortcutPressed(false), 1000)
+      }
     }
+
+    document.addEventListener('keydown', down)
+
+    return () => {
+      document.removeEventListener('keydown', down)
+
+      if (commandTimeoutRef.current) {
+        clearTimeout(commandTimeoutRef.current)
+      }
+    }
+  }, [])
+
+  async function handleRemoveBookmark() {
+    startTransition(async () => {
+      remove(bookmarkId)
+
+      const result = await actionRemoveBookmark(bookmarkId)
+
+      if (result.success === false) {
+        toast.error(result.message)
+        return
+      }
+    })
   }
 
   return (
-    <DropdownMenuItem>
+    <DropdownMenuItem asChild>
       <Button
         variant="ghost"
-        className="w-full h-fit m-0 p-0 justify-start border-none hover:bg-none font-medium"
-        onClick={() => startTransition(handleRemoveBookmark)}
+        className={cn('w-full relative cursor-pointer', shortcutPressed && 'bg-accent')}
+        onClick={handleRemoveBookmark}
         disabled={isPending}
       >
-        <Trash strokeWidth={1.25} className="size-4" />
-        Remove
+        <CircleXIcon
+          className={cn(
+            'absolute inset-x-2 inset-y-2.5 size-4 transition-all duration-200 ease-out',
+            shortcutPressed ? 'scale-75 opacity-0' : 'scale-100 opacity-100',
+          )}
+        />
+        <CircleMinusIcon
+          className={cn(
+            'absolute inset-x-2 inset-y-2.5 size-4 transition-all duration-200 ease-out',
+            shortcutPressed ? 'scale-100 opacity-100' : 'scale-75 opacity-0',
+          )}
+        />
+        <span className="relative left-6">Delete</span>
+        <DropdownMenuShortcut>⌘+⇧+D</DropdownMenuShortcut>
       </Button>
     </DropdownMenuItem>
   )
