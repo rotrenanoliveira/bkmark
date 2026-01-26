935fb323a49bd5a766d41cacf61431a7a70e3f29
Renan Oliveira <rotrenanoliveira@gmail.com>
2026-01-26 16:35:18 -0300

    refact: main folders component

--- src/components/folders/folders.tsx
+++ src/components/folders/folders.tsx
@@ -1,18 +1,23 @@
+import { redirect } from 'next/navigation'
 import React from 'react'
-
 import { getUserFolders } from '@/server/data/get-folders'
 import { getUserId } from '@/server/data/get-user-id'
 import { Folder } from './folder'
+import { LoadingFolder } from './loading-folder'
 
 export async function Folders() {
   const userId = await getUserId()
+
+  if (!userId) {
+    redirect('/')
+  }
+
   const { folders } = await getUserFolders(userId)
 
   return (
     <div className="space-y-2">
-      {/* registered folders */}
       {folders.map((folder) => (
-        <React.Suspense key={folder.folderId}>
+        <React.Suspense key={folder.folderId} fallback={<LoadingFolder />}>
           <Folder folder={folder} />
         </React.Suspense>
       ))}
