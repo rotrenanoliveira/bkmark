80673e86d1852ee23ff46ad1d66166474328816a
Renan Oliveira <rotrenanoliveira@gmail.com>
2026-01-28 14:35:21 -0300

    refact: rename component `BookmarkRename` to `RenameBookmark`

--- src/components/bookmarks/bookmark-options.tsx
+++ src/components/bookmarks/bookmark-options.tsx
@@ -3,10 +3,10 @@ import { Button } from '@/components/ui/button'
 import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
 import type { Bookmark } from '@/utils/types'
 import { BookmarkCopyUrl } from './bookmark-copy-url'
-import { BookmarkDeleteButton } from './bookmark-delete'
 import { BookmarkMoveSubMenu } from './bookmark-move-submenu'
 import { BookmarkRemoveFromFolderButton } from './bookmark-remove-from-folder'
-import { BookmarkRename } from './bookmark-rename'
+import { RenameBookmark } from './bookmark-rename'
+import { DeleteBookmark } from './delete-bookmark'
 
 interface BookmarkOptionsProps {
   bookmark: Bookmark
@@ -28,9 +28,9 @@ export function BookmarkOptions({ bookmark }: BookmarkOptionsProps) {
       <DropdownMenuContent className="w-56 space-y-1" align="end">
         <BookmarkCopyUrl bookmarkUrl={bookmark.bookmarkUrl} />
 
-        <BookmarkRename bookmarkId={bookmark.bookmarkId} />
+        <RenameBookmark bookmarkId={bookmark.bookmarkId} />
 
-        <BookmarkDeleteButton bookmarkId={bookmark.bookmarkId} />
+        <DeleteBookmark bookmarkId={bookmark.bookmarkId} />
 
         {hasParentFolder && <BookmarkRemoveFromFolderButton bookmarkId={bookmark.bookmarkId} />}
 
