d6a9ec883cb58228c03f282018b17f93294541e2
Renan Oliveira <rotrenanoliveira@gmail.com>
2026-01-29 10:40:06 -0300

    feat: using folder context

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -5,20 +5,25 @@ import { ThemeProvider } from 'next-themes'
 import type { ReactNode } from 'react'
 import { Toaster } from '@/components/ui/sonner'
 import { BookmarkProvider } from '@/contexts/bookmarks-context'
+import { FoldersProvider } from '@/contexts/folders-context'
 import { queryClient } from '@/lib/react-query'
-import type { Bookmark } from '@/utils/types'
+import type { Bookmark, Folder } from '@/utils/types'
 
 export function Providers({
   children,
   bookmarksPromise,
+  foldersPromise,
 }: {
   children: ReactNode
   bookmarksPromise: Promise<Bookmark[]>
+  foldersPromise: Promise<Folder[]>
 }) {
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider attribute="class" defaultTheme="light" disableTransitionOnChange>
-        <BookmarkProvider bookmarksPromise={bookmarksPromise}>{children}</BookmarkProvider>
+        <BookmarkProvider bookmarksPromise={bookmarksPromise}>
+          <FoldersProvider foldersPromise={foldersPromise}>{children}</FoldersProvider>
+        </BookmarkProvider>
         <Toaster richColors />
       </ThemeProvider>
     </QueryClientProvider>
