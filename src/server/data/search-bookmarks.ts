5e77878526fbeb84aa0c8c21a8edd479853984b5
Renan Oliveira <rotrenanoliveira@gmail.com>
2026-01-29 15:49:00 -0300

    fix: bookmarks search query condition

--- src/server/data/search-bookmarks.ts
+++ src/server/data/search-bookmarks.ts
@@ -1,6 +1,6 @@
 'use server'
 
-import { and, desc, eq, ilike, type SQL } from 'drizzle-orm'
+import { and, desc, eq, ilike, or } from 'drizzle-orm'
 import { db } from '@/infra/db/drizzle'
 import { bookmarksRepository } from '@/infra/db/repositories'
 import { handle } from '@/utils/functions'
@@ -9,38 +9,30 @@ import { getUserId } from './get-user-id'
 export async function searchBookmarks(search: string | null) {
   const userId = await getUserId()
 
-  if (!userId) {
-    return []
-  }
-
   if (!search) {
     return []
   }
 
-  const conditions: SQL[] = []
-
-  if (search) {
-    conditions.push(
-      ilike(bookmarksRepository.title, `%${search}%`),
-      ilike(bookmarksRepository.bookmarkUrl, `%${search}%`),
-    )
-  }
-
-  const [bookmarks, queryError] = await handle(
+  const [bookmarks, bookmarksQueryError] = await handle(
     db
       .select({
-        bookmarkId: bookmarksRepository.bookmarkId,
-        bookmarkUrl: bookmarksRepository.bookmarkUrl,
+        id: bookmarksRepository.bookmarkId,
+        url: bookmarksRepository.bookmarkUrl,
         title: bookmarksRepository.title,
         favicon: bookmarksRepository.favicon,
       })
       .from(bookmarksRepository)
-      .where(and(...conditions, eq(bookmarksRepository.userId, userId)))
+      .where(
+        and(
+          eq(bookmarksRepository.userId, userId),
+          or(ilike(bookmarksRepository.title, `%${search}%`), ilike(bookmarksRepository.bookmarkUrl, `%${search}%`)),
+        ),
+      )
       .orderBy(desc(bookmarksRepository.createdAt)),
   )
 
-  if (queryError) {
-    throw queryError.message
+  if (bookmarksQueryError) {
+    throw bookmarksQueryError.message
   }
 
   return bookmarks
