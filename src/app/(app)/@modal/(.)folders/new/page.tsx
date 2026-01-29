020c188e29f5a7d8c889b245d9665c4da77dca36
Renan Oliveira <rotrenanoliveira@gmail.com>
2026-01-29 10:44:23 -0300

    refact: create folder, page and dialog

--- src/app/(app)/@modal/(.)folders/new/page.tsx
+++ src/app/(app)/@modal/(.)folders/new/page.tsx
@@ -1,5 +1,21 @@
-import { AddFolderDialogContent } from './_components/add-folder-dialog-content'
+'use client'
 
-export default async function AddFolderModal() {
-  return <AddFolderDialogContent />
+import { CreateFolderContent } from '@/components/folders/create-folder-content'
+import { DialogDescription, DialogHeader, DialogTitle } from '@/components/ui/dialog'
+import { InterceptDialog } from '@/components/ui/intercept-dialog'
+import { useInterceptModal } from '@/hooks/use-intercept-modal'
+
+export default function CreateFolderInterceptDialog() {
+  const { open, setOpen, handleClose } = useInterceptModal()
+
+  return (
+    <InterceptDialog open={open} setOpen={setOpen}>
+      <DialogHeader className="sr-only">
+        <DialogTitle className="text-center">Register a new folder</DialogTitle>
+        <DialogDescription>Enter the name of the folder you want to add.</DialogDescription>
+      </DialogHeader>
+
+      <CreateFolderContent onClose={handleClose} />
+    </InterceptDialog>
+  )
 }
