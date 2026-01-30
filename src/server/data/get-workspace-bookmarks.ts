d8ea1e67371d0eb3258bb824346b9b6ddec0d40d
Renan Oliveira <rotrenanoliveira@gmail.com>
2026-01-30 11:36:56 -0300

    feat: add get workspace bookmarks

---
+++ src/server/data/get-workspace-bookmarks.ts
@@ -0,0 +1,41 @@
+import { and, desc, eq } from 'drizzle-orm'
+import { cacheRepository } from '@/infra/cache/cache-repository'
+import { db } from '@/infra/db/drizzle'
+import { bookmarksRepository } from '@/infra/db/repositories'
+import { handle } from '@/utils/functions'
+import type { BookmarkPresenter } from '@/utils/types'
+
+export async function getWorkspaceBookmarks(userId: string, workspaceId: string) {
+  const [bookmarksOnCache, _getCacheError] = await handle(
+    cacheRepository.get<BookmarkPresenter[]>(`${userId}:workspace:${workspaceId}`),
+  )
+
+  if (bookmarksOnCache) {
+    return bookmarksOnCache
+  }
+
+  const [bookmarks, queryError] = await handle(
+    db
+      .select({
+        userId: bookmarksRepository.userId,
+        bookmarkId: bookmarksRepository.bookmarkId,
+        bookmarkUrl: bookmarksRepository.bookmarkUrl,
+        favicon: bookmarksRepository.favicon,
+        title: bookmarksRepository.title,
+        folderId: bookmarksRepository.folderId,
+        workspaceId: bookmarksRepository.workspaceId,
+        createdAt: bookmarksRepository.createdAt,
+      })
+      .from(bookmarksRepository)
+      .where(and(eq(bookmarksRepository.userId, userId), eq(bookmarksRepository.workspaceId, workspaceId)))
+      .orderBy(desc(bookmarksRepository.createdAt)),
+  )
+
+  if (queryError) {
+    throw queryError.message
+  }
+
+  await handle(cacheRepository.set(`${userId}:workspace:${workspaceId}`, JSON.stringify(bookmarks)))
+
+  return bookmarks
+}
