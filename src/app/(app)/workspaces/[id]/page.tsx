a220d4d06ee7a9f8c52457fcb40e620390bf6601
Renan Oliveira <rotrenanoliveira@gmail.com>
2026-02-03 15:15:24 -0300

    refact: create bookmark in workspaces page

--- src/app/(app)/workspaces/[id]/page.tsx
+++ src/app/(app)/workspaces/[id]/page.tsx
@@ -1,17 +1,15 @@
 import { BookmarksList } from '@/components/bookmarks/bookmarks-list'
+import { CreateBookmarkForm } from '@/components/bookmarks/create-bookmark-form'
 import { FoldersList } from '@/components/folders/folders-list'
 import { Header } from '@/components/header'
-import { CreateBookmark } from './_components/create-bookmark'
 
-export default async function WorkspacePage({ params }: { params: Promise<{ id: string }> }) {
-  const { id: workspaceId } = await params
-
+export default function WorkspacePage() {
   return (
-    <div className="flex flex-col min-h-screen w-screen">
+    <div className="flex flex-col w-screen min-h-screen">
       <Header />
-      <main className="w-full flex flex-col items-center p-6 space-y-4">
-        <section className="w-full max-w-4xl grid grid-cols-1 gap-4">
-          <CreateBookmark workspaceId={workspaceId} />
+      <main className="flex flex-col items-center w-full p-6 space-y-4">
+        <section className="grid w-full max-w-4xl grid-cols-1 gap-4">
+          <CreateBookmarkForm />
 
           <BookmarksList />
           <FoldersList />
