b7d4ef16569fb2422953b035a7dc48725a58804b
Renan Oliveira <rotrenanoliveira@gmail.com>
2026-02-03 15:15:09 -0300

    refact: create bookmark in folders page

--- src/app/(app)/folders/[id]/page.tsx
+++ src/app/(app)/folders/[id]/page.tsx
@@ -1,16 +1,14 @@
 import { BookmarksList } from '@/components/bookmarks/bookmarks-list'
+import { CreateBookmarkForm } from '@/components/bookmarks/create-bookmark-form'
 import { Header } from '@/components/header'
-import { CreateBookmark } from './_components/create-bookmark'
 
-export default async function FolderPage({ params }: { params: Promise<{ id: string }> }) {
-  const { id: folderId } = await params
-
+export default async function FolderPage() {
   return (
     <div className="flex flex-col min-h-screen w-screen">
       <Header />
       <main className="w-full flex flex-col items-center p-6 space-y-4">
         <section className="w-full max-w-4xl grid grid-cols-1 gap-4">
-          <CreateBookmark folderId={folderId} />
+          <CreateBookmarkForm />
 
           <BookmarksList />
         </section>
