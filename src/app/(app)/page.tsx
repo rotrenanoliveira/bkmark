0cff18f613c48e29d816b955500b9abc2a1609ad
Renan Oliveira <rotrenanoliveira@gmail.com>
2026-02-03 14:56:00 -0300

    feat: remove folder and workspaces create form from home page

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,9 +1,7 @@
 import { BookmarksList } from '@/components/bookmarks/bookmarks-list'
 import { CreateBookmarkForm } from '@/components/bookmarks/create-bookmark-form'
-import { CreateFolderForm } from '@/components/folders/create-folder-form'
 import { FoldersList } from '@/components/folders/folders-list'
 import { Header } from '@/components/header'
-import { CreateWorkspaceForm } from '@/components/workspaces/create-workspace-form'
 import { WorkspaceList } from '@/components/workspaces/workspace-list'
 
 export default async function HomePage() {
@@ -13,8 +11,6 @@ export default async function HomePage() {
       <main className="w-full flex flex-col items-center p-6 space-y-4">
         <section className="w-full max-w-4xl grid grid-cols-1 gap-4">
           <CreateBookmarkForm />
-          <CreateFolderForm />
-          <CreateWorkspaceForm />
 
           <WorkspaceList />
           <BookmarksList />
