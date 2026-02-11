0fa46a8ad8e747be9ba16f21373cac050e604432
Renan Oliveira <rotrenanoliveira@gmail.com>
2026-02-11 13:14:21 -0300

    fix: infine loading on `BookmarkOptions` when user doesn't have folder or workspace

--- src/components/bookmarks/move-bookmarks.tsx
+++ src/components/bookmarks/move-bookmarks.tsx
@@ -1,4 +1,4 @@
-import { CornerDownRightIcon, Loader2Icon } from 'lucide-react'
+import { CornerDownRightIcon } from 'lucide-react'
 import React from 'react'
 import {
   DropdownMenuItem,
@@ -45,16 +45,6 @@ export function MoveBookmarks({ bookmarkId, currentFolder, currentWorkspace }: M
                 )}
                 <ScrollArea className={cn('w-full rounded-md', folders.length > 7 ? 'h-72' : 'h-fit')}>
                   <div className={cn('space-y-0.5', folders && folders.length > 0 && 'p-1')}>
-                    {folders?.length === 0 && (
-                      <DropdownMenuItem
-                        className="relative h-9 px-4 py-2 has-[>svg]:px-3 text-sm font-medium transition-all outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50 disabled:pointer-events-none disabled:opacity-50"
-                        disabled
-                      >
-                        <Loader2Icon className="animate-spin absolute inset-x-2 inset-y-2.5 size-4 transition-all duration-200 ease-out scale-100 opacity-100" />
-                        <span className="relative left-6">Loading...</span>
-                      </DropdownMenuItem>
-                    )}
-
                     {folders &&
                       folders.length > 0 &&
                       folders.map((folder) => (
@@ -88,16 +78,6 @@ export function MoveBookmarks({ bookmarkId, currentFolder, currentWorkspace }: M
 
                 <ScrollArea className={cn('w-full rounded-md', workspaces.length > 7 ? 'h-72' : 'h-fit')}>
                   <div className={cn('space-y-0.5', workspaces && workspaces.length > 0 && 'p-1')}>
-                    {workspaces.length === 0 && (
-                      <DropdownMenuItem
-                        className="relative h-9 px-4 py-2 has-[>svg]:px-3 text-sm font-medium transition-all outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50 disabled:pointer-events-none disabled:opacity-50"
-                        disabled
-                      >
-                        <Loader2Icon className="animate-spin absolute inset-x-2 inset-y-2.5 size-4 transition-all duration-200 ease-out scale-100 opacity-100" />
-                        <span className="relative left-6">Loading...</span>
-                      </DropdownMenuItem>
-                    )}
-
                     {workspaces &&
                       workspaces.length > 0 &&
                       workspaces.map((workspace) => (
